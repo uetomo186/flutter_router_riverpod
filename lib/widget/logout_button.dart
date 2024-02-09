26776892d9569089ed1f099ea322650895bbbb08
uetomo186 <hagurono9@gmail.com>
2024-02-09 14:29:28 +0900

    ログアウトボタンを作成

--- lib/widget/logout_button.dart
+++ lib/widget/logout_button.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_router_riverpod/state/auth_controller.dart';
+import 'package:flutter_router_riverpod/widget/action_button.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class LogoutButton extends ConsumerWidget {
@@ -6,6 +8,10 @@ class LogoutButton extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return ActionButton();
+    return ActionButton(
+      onPressed: ref.read(authControllerProvider.notifier).logout,
+      icon: const Icon(Icons.logout),
+      label: const Text('ログアウト'),
+    );
   }
 }
