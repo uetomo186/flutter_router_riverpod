a5e19379a81d38dd2979fcaf53eead84b4839e23
uetomo186 <hagurono9@gmail.com>
2024-02-09 15:55:29 +0900

    nuclear_codes.dartを追加

---
+++ lib/state/nuclear_codes.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'nuclear_codes.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$nuclearCodesHash() => r'c3257c244d00e8c15bc4bd3fd5ce536e59d6062c';
+
+/// See also [nuclearCodes].
+@ProviderFor(nuclearCodes)
+final nuclearCodesProvider =
+    AutoDisposeFutureProvider<List<(IconData, String)>>.internal(
+  nuclearCodes,
+  name: r'nuclearCodesProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$nuclearCodesHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef NuclearCodesRef
+    = AutoDisposeFutureProviderRef<List<(IconData, String)>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
