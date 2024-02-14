0a9cdcdbd114daabec8e39b9eaf914a1f3367cb7
uetomo186 <hagurono9@gmail.com>
2024-02-14 12:00:03 +0900

    ユーザーページを作成

--- lib/pages/user_page.dart
+++ lib/pages/user_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_router_riverpod/state/cute_rabbits.dart';
+import 'package:flutter_router_riverpod/widget/logout_button.dart';
+import 'package:flutter_router_riverpod/widget/my_sliver_list.dart';
+import 'package:flutter_router_riverpod/widget/user_title.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class UserPage extends ConsumerWidget {
@@ -6,13 +10,42 @@ class UserPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return  Scaffold(
+    final rabbits = ref.watch(cuteRabbitsProvider);
+
+    return Scaffold(
       body: SafeArea(
-   
-        child: Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children[]),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 24),
+                  child: Text('ユーザーページ',
+                      style: TextStyle(fontWeight: FontWeight.bold)),
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  "ウサギを拾うのに十分な権限を持っているようです！",
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 20),
+                Flexible(
+                  child: CustomScrollView(
+                    slivers: [
+                      const SliverToBoxAdapter(child: UserTitle()),
+                      MySliverList(elements: rabbits),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 40),
+                const LogoutButton(),
+              ],
+            ),
+          ),
         ),
-      )),
+      ),
     );
   }
 }
