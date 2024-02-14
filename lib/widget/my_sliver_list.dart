d690cb56823a07ba2264b3073d6555e5f81ac1aa
uetomo186 <hagurono9@gmail.com>
2024-02-14 12:36:51 +0900

    定義

--- lib/widget/my_sliver_list.dart
+++ lib/widget/my_sliver_list.dart
@@ -1,5 +1,6 @@
 import 'dart:math' as math;
 import 'package:flutter/material.dart';
+import 'package:flutter_router_riverpod/router/routes.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class MySliverList extends StatelessWidget {
